c9fd0c3c57058ddd57e3b6dfa9d248860bbab37e
MILAN V J P <vithayathil.m97@htlwienwest.at>
2020-12-16 20:05:33 +0100

    Initial code for the GitSampleProject project.

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+
+        Sample.Lamp led = new Sample.Lamp();
+        Sample.Lamp halogen = new Sample.Lamp();
+
+        led.turnOn();
+        halogen.turnOn();
+        led.turnOff();
+        halogen.turnOff();
+    }
+}
---
+++ src/com/company/Sample.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Sample
+{
+    static class Lamp
+    {
+        boolean isOn;
+
+        void turnOn()
+        {
+            isOn = true;
+            System.out.println("Light on? " + isOn);
+
+        }
+
+        void turnOff()
+        {
+            isOn = false;
+            System.out.println("Light on? " + isOn);
+        }
+    }
+}
