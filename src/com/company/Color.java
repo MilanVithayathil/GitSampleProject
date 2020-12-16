81ee6e1c9753ff972840cb563adbaf9c48654f40
MILAN V J P <vithayathil.m97@htlwienwest.at>
2020-12-16 20:22:46 +0100

    Added attributes, constructor and methods to Color class.

--- src/com/company/Color.java
+++ src/com/company/Color.java
@@ -1,4 +1,22 @@
 package com.company;
 
-public class Color {
+public class Color
+{
+    private String lampColor;
+
+    public Color(String lampColor)
+    {
+        this.lampColor = lampColor;
+    }
+
+    public String getLampColor()
+    {
+        return lampColor;
+    }
+
+    public void setLampColor(String lampColor)
+    {
+        this.lampColor = lampColor;
+    }
 }
+
