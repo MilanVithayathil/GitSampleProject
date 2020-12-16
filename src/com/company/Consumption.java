a54cdda4098b5fae3950609c36e1db998c5eaa60
MILAN V J P <vithayathil.m97@htlwienwest.at>
2020-12-16 20:57:18 +0100

    Added an attribute and a constuctor to the Consumption class.

--- src/com/company/Consumption.java
+++ src/com/company/Consumption.java
@@ -1,4 +1,11 @@
 package com.company;
 
-public class Consumption {
+public class Consumption
+{
+    private int energyLv = 0;
+
+    public Consumption(int energyLv)
+    {
+        this.energyLv = energyLv;
+    }
 }
