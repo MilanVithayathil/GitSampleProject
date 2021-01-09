5e0ce6579d108c8baa7d3002c7d7b9ed7a886166
MILAN V J P <vithayathil.m97@htlwienwest.at>
2021-01-09 15:34:41 +0100

    Deleted the catch block from Main due to recommendation.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,6 @@ public class Main
 {
     public static void main(String[] args)
     {
-        try {
             System.out.println("Hello World");
 
             Sample.Lamp led = new Sample.Lamp();
@@ -12,9 +11,5 @@ public class Main
 
             led.turnOn();
             halogen.turnOn();
-        }
-        catch (ArithmeticException e){
-            System.out.println("error");
-        }
     }
 }
